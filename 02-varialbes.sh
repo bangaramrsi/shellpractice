00f1f5dae49cd498ebfea2e46be20dd97f4ce29b
bangaramrsi <bangaramrsi@gmail.com>
2024-05-16 12:25:28 +0530

    practice in office variable file

--- 02-varialbes.sh
+++ 02-varialbes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-PERSON1=JAGAN
-PERSON2=VIJAY
+PERSON1=jagan
+PERSON2=vijay
 
 
 echo "$PERSON1: hi $PERSON2 where r u"
-echo "v$PERSON2: hi $PERSON1 i am in Delhi"
+echo "$PERSON2: hi $PERSON1 i am in Delhi"
 echo "$PERSON1: when will u come back to hyderabad"
 echo "$PERSON2: next week i will come back to hyderabad"
