d727328b6bf25842980217c5f6b157098ad5c863
bangaramrsi <bangaramrsi@gmail.com>
2024-05-16 12:19:48 +0530

    practice in office variable file

--- variables.sh
+++ variables.sh
@@ -3,4 +3,4 @@
 echo "jagan: hi vijay where r u"
 echo "vijay: hi jagan i am in Delhi"
 echo "jagan: when will u come back to hyderabad"
-echo "vijay": next week i will come back to hyderabad"
+echo "vijay: next week i will come back to hyderabad"
