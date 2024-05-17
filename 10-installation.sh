32bfafd2e7ba594915f69be343624edb9461cb2b
bangaramrsi <bangaramrsi@gmail.com>
2024-05-17 15:15:35 +0530

    practice in office variable file

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
 
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with super user"
-else
-    echo "current user is super user"
-fi
-
-yum install mysql -y
-
